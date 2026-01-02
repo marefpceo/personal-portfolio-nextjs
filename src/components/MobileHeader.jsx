9709a1c4b7ea94182798754fdd7af491e310aef5
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-02 07:59:16 -0800

    Updated MobileHeader styling

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import Logo1 from '../../public/Logo1.svg';
 import { RxHamburgerMenu } from 'react-icons/rx'
@@ -5,14 +7,17 @@ import { RxHamburgerMenu } from 'react-icons/rx'
 export default function MobileHeader() {
 
   return (
-    <header className='flex justify-between'>
+    <header className='p-1 flex justify-between'>
       <Image
         loading='eager'
         src={Logo1}
         alt='test Image'
         width={120}
       />
-      <RxHamburgerMenu className='text-nature-light size-6 mx-6 my-2' />
+      <RxHamburgerMenu 
+        className='text-nature-light size-6 mx-6 my-2' 
+        onClick={() => {console.log('Toggle Menu')}}
+      />
     </header>
-  );
+  )
 }
