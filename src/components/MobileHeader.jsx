db8ad43a82f3c089c218dcd0e5415f7375c937e4
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-02 06:42:04 -0800

    Created stand alone component MobileHeader

---
+++ src/components/MobileHeader.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Logo1 from '../../public/Logo1.svg';
+import { RxHamburgerMenu } from 'react-icons/rx'
+
+export default function MobileHeader() {
+
+  return (
+    <header className='flex justify-between'>
+      <Image
+        loading='eager'
+        src={Logo1}
+        alt='test Image'
+        width={120}
+      />
+      <RxHamburgerMenu className='text-nature-light size-6 mx-6 my-2' />
+    </header>
+  );
+}
