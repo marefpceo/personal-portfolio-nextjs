f9efbdf967ef74bd37b075be4246ce5751045e45
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-04 09:48:21 -0800

    Working on ProjectCard carousel

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,22 @@
 import Image from 'next/image';
 
-export default function ProjectCard() {
+export default function ProjectCard({ projectImage }) {
 
   return (
-    <div>
-
+    <div className='mt-2 p-4 w-4/5 min-h-3/5 flex flex-col items-center gap-4 border rounded-2xl bg-nature-light 
+      text-nature-dark border-nature-accent shadow-sm shadow-nature-dark'
+    >
+      <Image 
+        className='my-2 w-11/12 rounded-xl shadow-sm shadow-nature-alt'
+        loading='eager'
+        src={projectImage}
+        alt={"placeholder"}
+        width={960}
+        height={746}
+      />
+      <h3 className='text-lg'>Project Title</h3>
+      <span>(Tech stack text bubble)</span>
+      <p className='text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat, alias possimus voluptatibus hic velit!</p>
     </div>
   )
 }
--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -3,10 +3,22 @@ import ProjectCard from '@/components/ProjectCard';
 export default function ProjectsSection () {
 
   return (
-    <section id='projects' className='py-6 px-4 flex flex-col flex-1 bg-nature-alt text-nature-light'>
+    <section id='projects' className='py-6 px-4 h-full flex flex-col gap-8 items-center bg-nature-alt text-nature-light'>
       <h2 className='text-center text-3xl'>Projects</h2>
 
-      <ProjectCard />
+      <ProjectCard 
+        projectImage={'/image_placeholder.png'}
+      />
+      <ProjectCard 
+        projectImage={'/image_placeholder.png'}
+      />
+      <ProjectCard 
+        projectImage={'/image_placeholder.png'}
+      />
+      <ProjectCard 
+        projectImage={'/image_placeholder.png'}
+      />
+     
     </section>
   );
 }
