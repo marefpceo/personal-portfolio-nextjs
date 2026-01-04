2e2a9b0fa3be68edc43246bd182de41ddafa4f78
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-03 16:28:11 -0800

    Created ProjectCard shell

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -1,8 +1,12 @@
+import ProjectCard from '@/components/ProjectCard';
+
 export default function ProjectsSection () {
 
   return (
-    <section id='projects' className='bg-nature-alt text-nature-light'>
-      Projects
+    <section id='projects' className='py-6 px-4 flex flex-col flex-1 bg-nature-alt text-nature-light'>
+      <h2 className='text-center text-3xl'>Projects</h2>
+
+      <ProjectCard />
     </section>
   );
 }
