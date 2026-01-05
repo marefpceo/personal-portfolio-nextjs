47a9eb40410025d1bfe5f8cd72971845a5335da5
marefpceo <marefpceo@gmail.com>
2026-01-04 20:48:36 -0800

    Started ContactSection layout

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -1,7 +1,16 @@
 export default function ContactSection() {
   return (
-    <section id='contact' className='bg-nature-dark text-nature-light'>
-      Contact
+    <section id='contact' className='py-6 px-2 min-h-lvh flex flex-col items-center bg-nature-dark text-nature-light'>
+      <h2 className='mb-8 text-center text-3xl'>Contact</h2>
+
+      <div className='mt-10 w-3/4 flex flex-col gap-8'>
+        <input type="text" name="subject" id="subject" className='bg-nature-light rounded-lg'/>
+        <textarea name="message" id="message" cols="30" rows="10" className='bg-nature-light rounded-lg'></textarea>
+      </div>
+
+      <div className='mt-8'>
+        <button type="button">Submit</button>
+      </div>
     </section>
   );
 }
