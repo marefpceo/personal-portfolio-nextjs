93adc02d372d46b5487131d6071c8118881b614e
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-12 10:30:24 -0800

    Resized images for a uniformed fit

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,20 +9,25 @@ export default function ProjectCard({ projectTitle, projectImage, projectDescrip
   return (
     <div
       className='mt-2 p-4 max-w-11/12 min-h-3/5 flex flex-col items-center gap-4 border rounded-2xl bg-nature-light 
-      text-nature-dark border-nature-accent shadow-sm shadow-nature-dark md:flex-row md:even:flex-row-reverse
+      text-nature-dark border-nature-accent shadow-sm shadow-nature-dark md:grid md:grid-cols-3
       md:w-11/12 md:h-60'
     >
-      <Image
-        className='my-2 w-11/12 md:max-w-1/4 rounded-sm shadow-sm shadow-nature-alt'
-        loading='eager'
-        src={projectImage}
-        alt={'placeholder'}
-        width={960}
-        height={746}
-      />
-      <div className='flex flex-col gap-3 items-center'>
+      <div className='md:flex md:flex-col md:items-center md:self-start'>
+        <Image
+          className='my-2 w-11/12 md:max-w-48 md:max-h-32 rounded-sm shadow-sm shadow-nature-alt'
+          loading='eager'
+          src={projectImage}
+          alt={'placeholder'}
+          width={960}
+          height={746}
+        />
+        <span className='not-sm:hidden mt-4'>links</span>
+      </div>
+      <div className='flex flex-col justify-center text-center md:grid md:grid-rows-2 md:self-start
+        md:col-span-2 md:gap-2'
+      >
         <h3 className='text-2xl'>{projectTitle}</h3>
-        <span className='my-3 flex gap-4 flex-wrap'>
+        <span className='my-2 flex gap-2 flex-wrap justify-center'>
         {
           techStack.map(tech => (
             <TechTextBubble 
@@ -31,9 +36,10 @@ export default function ProjectCard({ projectTitle, projectImage, projectDescrip
           ))
         }
         </span>
-        <p className='text-center text-sm'>
+        <p className='mt-2 text-center text-sm overflow-y-scroll'>
           {projectDescription}
         </p>
+        <span className='md:hidden md:mt-2'>links</span>
       </div>
     </div>
   );
