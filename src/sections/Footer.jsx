edf03ca44294e2b0839a4cf866c6e9c468dd4b18
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-01-16 15:53:15 -0800

    Competed Footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import linkedIn from '../../public/icons/linkedIn-dark.png' 
+import github from '../../public/icons/github-dark.png'
+
 export default function Footer() {
 
   return (
-    <footer className='min-h-20 flex justify-center items-center bg-nature-light text-nature-dark'>
-      <p className='text-sm'>Copyright &copy; 2026</p>
+    <footer className='min-h-24 relative flex flex-col justify-center items-center bg-nature-light text-nature-dark'>
+      <div className='my-4 flex gap-6'>
+        <Link href={'https://www.github.com/marefpceo'} target='_blank'>
+          <Image
+            src={github}
+            alt='Github contact button'
+            width={25}
+            height={25}
+          />
+        </Link>
+        <Link href={'https://www.linkedin.com/in/lamarstevens'} target='_blank'>
+          <Image
+            src={linkedIn}
+            alt='LinkedIn contact button'
+            width={25}
+            height={25}
+          />
+        </Link>
+      </div>
+      <p className='text-sm '>&copy; 2026 Lamar Stevens</p>
     </footer>
   )
 }
