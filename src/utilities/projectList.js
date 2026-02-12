b78958d670975a377d9c5c6d6535a4a2b4e11630
marefpceo <marefpceo@gmail.com>
2026-02-11 19:47:24 -0800

    Updated link to live demo for Messaging App

--- src/utilities/projectList.js
+++ src/utilities/projectList.js
@@ -4,59 +4,70 @@ export const projectList = [
     projectDescription: 'Description of project',
     projectImage: '/lastStopShop.webp',
     projectImageUrl: '/lastStopShop.webp',
-    projectLinks: [{
-      name: 'Live Demo',
-      url: 'https://react-shopping-cart-4ft.pages.dev/',
-    },
-    {
-      name: 'Source Code',
-      url: 'https://github.com/marefpceo/react-shopping-cart',
-    }],
+    projectLinks: [
+      {
+        name: 'Live Demo',
+        url: 'https://react-shopping-cart-4ft.pages.dev/',
+      },
+      {
+        name: 'Source Code',
+        url: 'https://github.com/marefpceo/react-shopping-cart',
+      },
+    ],
     techStack: ['React', 'Vite', 'CSS'],
   },
   {
     projectTitle: 'Express Inventory App',
-    projectDescription: 'Express Inventory App is an inventory management web application designed for a fictitious grocery store. The app makes use of CRUD methods for managing, maintaining and updating the store\'s inventory. The app aims to give users a quick and efficient way view and manage inventories, organizing items by categories and subcategories',
+    projectDescription:
+      "Express Inventory App is an inventory management web application designed for a fictitious grocery store. The app makes use of CRUD methods for managing, maintaining and updating the store's inventory. The app aims to give users a quick and efficient way view and manage inventories, organizing items by categories and subcategories",
     projectImage: '/expressInventoryApp.webp',
     projectImageUrl: '/expressInventoryApp.webp',
-    projectLinks: [{
-      name: 'Live Demo',
-      url: '#',
-    },
-    {
-      name: 'Source Code',
-      url: 'https://github.com/marefpceo/express-inventory-app-v2',
-    }],
+    projectLinks: [
+      {
+        name: 'Live Demo',
+        url: '#',
+      },
+      {
+        name: 'Source Code',
+        url: 'https://github.com/marefpceo/express-inventory-app-v2',
+      },
+    ],
     techStack: ['NodeJS', 'ExpressJS', 'PostgreSQL', 'EJS'],
   },
   {
     projectTitle: 'Blog API (Client)',
-    projectDescription: 'Blog API is a project used to create a blog site using an API only backend and two different front-ends for accessing and editing blog posts.',
+    projectDescription:
+      'Blog API is a project used to create a blog site using an API only backend and two different front-ends for accessing and editing blog posts.',
     projectImage: '/blogClientHome.webp',
     projectImageUrl: '/blogClientHome.webp',
-    projectLinks: [{
-      name: 'Live Demo',
-      url: 'https://blog-api-c9w.pages.dev/',
-    },
-    {
-      name: 'Source Code',
-      url: 'https://github.com/marefpceo/blog-api'
-    }],
+    projectLinks: [
+      {
+        name: 'Live Demo',
+        url: 'https://blog-api-c9w.pages.dev/',
+      },
+      {
+        name: 'Source Code',
+        url: 'https://github.com/marefpceo/blog-api',
+      },
+    ],
     techStack: ['React', 'Vite', 'Tailwind', 'ExpressJS'],
   },
   {
     projectTitle: 'Blog API (Admin)',
-    projectDescription: 'Blog API is a project used to create a blog site using an API only backend and two different front-ends for accessing and editing blog posts.',
+    projectDescription:
+      'Blog API is a project used to create a blog site using an API only backend and two different front-ends for accessing and editing blog posts.',
     projectImage: '/blogAdminLogin.webp',
     projectImageUrl: '/blogAdminLogin.webp',
-    projectLinks: [{
-      name: 'Live Demo',
-      url: 'https://blog-api-admin.pages.dev/login',
-    },
-    {
-      name: 'Source Code',
-      url: 'https://github.com/marefpceo/blog-api',
-    }],
+    projectLinks: [
+      {
+        name: 'Live Demo',
+        url: 'https://blog-api-admin.pages.dev/login',
+      },
+      {
+        name: 'Source Code',
+        url: 'https://github.com/marefpceo/blog-api',
+      },
+    ],
     techStack: ['React', 'Vite', 'Tailwind', 'ExpressJS'],
   },
   {
@@ -66,13 +77,14 @@ export const projectList = [
     projectImageUrl: '/messagingHome.webp',
     projectLinks: [
       {
-      name: 'Live Demo',
-      url: '#',
-    },
-    {
-      name: 'Source Code',
-      url: 'https://github.com/marefpceo/messaging-app-server'
-    }],
+        name: 'Live Demo',
+        url: 'https://messaging-app-frontend-7ib.pages.dev/',
+      },
+      {
+        name: 'Source Code',
+        url: 'https://github.com/marefpceo/messaging-app-server',
+      },
+    ],
     techStack: ['NodeJS', 'ExpressJS', 'PostgreSQL', 'Prisma ORM'],
-  }
+  },
 ];
