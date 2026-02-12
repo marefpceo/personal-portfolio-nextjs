b8c904842caece05aa61a83bc5f12f6cc2a9d8d7
marefpceo <marefpceo@gmail.com>
2026-02-11 17:39:34 -0800

    Added project root path

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  turbopack: {
+    root: '/home/marefpceo/repos/personal/personal-portfolio-nextjs/',
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default nextConfig;
