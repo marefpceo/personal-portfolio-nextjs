c60a695b6bcc547ab4a5c78265c3642ee4d1e9b2
marefpceo <marefpceo@gmail.com>
2026-02-11 19:37:28 -0800

    Removed linstagedrc file causing conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import eslintNextPlugin from '@next/eslint-plugin-next';
 import prettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
@@ -8,11 +9,22 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    files: ['**/*.{js,jsx,ts,tsx'],
+    plugins: {
+      next: eslintNextPlugin,
+    },
+    settings: {
+      next: {
+        rootDir: 'packages/personal-portfolio-nextjs',
+      },
+    },
+  },
 ]);
 
 export default eslintConfig;
