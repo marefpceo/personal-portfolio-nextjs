85db6d76c569701a291720c536eb49ed5381cb47
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-02-16 15:42:56 -0800

    Added swipe functionality to Carousel component

--- src/utilities/Carousel.jsx
+++ src/utilities/Carousel.jsx
@@ -1,36 +1,47 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect, useRef } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 export default function Carousel({ children }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [items, setItems] = useState([]);
   const carouselRef = useRef(null);
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => nextSlide(),
+    onSwipedRight: () => prevSlide(),
+    preventScrollOnSwipe: true,
+    trackTouch: true,
+  });
+
   useEffect(() => {
     setItems(React.Children.toArray(children));
   }, [children]);
 
   function nextSlide() {
-    setCurrentIndex((prevIndex) => 
-      prevIndex === items.length - 1 ? 0 : prevIndex + 1
+    setCurrentIndex((prevIndex) =>
+      prevIndex === items.length - 1 ? 0 : prevIndex + 1,
     );
-  };
+  }
 
   function prevSlide() {
-    setCurrentIndex((prevIndex) => 
-      prevIndex === 0 ? items.length - 1 : prevIndex - 1
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? items.length - 1 : prevIndex - 1,
     );
-  };
+  }
 
   return (
-    <div className="carousel-container overflow-hidden w-full h-full relative">
-      <div className="carousel-wrapper flex h-3/4 relative">
-       
-        <div ref={carouselRef} className='carousel-content-wrapper overflow-hidden w-full'>
-          <div 
+    <div className='carousel-container overflow-hidden w-full h-full relative'>
+      <div className='carousel-wrapper flex h-3/4 relative'>
+        <div
+          ref={carouselRef}
+          className='carousel-content-wrapper overflow-hidden w-full'
+        >
+          <div
+            {...handlers}
             className='carousel-content flex transform duration-500 ease-in-out'
-            style={{ transform: `translateX(-${currentIndex * 100}%)`}}
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
           >
             {items.map((item, index) => (
               <div key={index} className='carousel-slide shrink-0 w-full'>
@@ -41,13 +52,17 @@ export default function Carousel({ children }) {
         </div>
       </div>
       <div className='my-6 h-1/4 items-center justify-center flex gap-16'>
-        <button onClick={prevSlide} className='left-arrow w-16 p-2.5 z-50 rounded-4xl bg-nature-dark 
+        <button
+          onClick={prevSlide}
+          className='left-arrow w-16 p-2.5 z-50 rounded-4xl bg-nature-dark 
           border  border-nature-light/50 text-nature-accent shadow-[0_0_8px_0] 
           shadow-nature-accent'
         >
           &#10094;
         </button>
-        <button onClick={nextSlide} className='right-arrow w-16 p-2.5 z-50 rounded-4xl bg-nature-dark
+        <button
+          onClick={nextSlide}
+          className='right-arrow w-16 p-2.5 z-50 rounded-4xl bg-nature-dark
           border  border-nature-light/50 text-nature-accent shadow-[0_0_8px_0] 
           shadow-nature-accent'
         >
@@ -55,5 +70,5 @@ export default function Carousel({ children }) {
         </button>
       </div>
     </div>
-  )
+  );
 }
